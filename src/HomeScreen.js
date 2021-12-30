a1220c0353e5a3f40d99ecc2c6db3a62114bfe54
Mitul Pancholi <mitul.pancholi@live.com>
2021-12-30 09:34:43 +0530

    initial setup

---
+++ src/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from './Banner'
+import './HomeScreen.css'
+import Nav from './Nav'
+const HomeSreen = () => {
+    return (
+        <div className='homeScreen'>
+            <Nav />
+           
+            <Banner />
+            {/* Row */}
+        </div>
+    )
+}
+
+export default HomeSreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_WitQBGNI9DY8HA4zvW3cKK0_t1GGhQs",
+    authDomain: "netflix-clone-33e37.firebaseapp.com",
+    projectId: "netflix-clone-33e37",
+    storageBucket: "netflix-clone-33e37.appspot.com",
+    messagingSenderId: "1002414174809",
+    appId: "1:1002414174809:web:8c2d8501ae681bcb517748"
+  };
